e67b45d7a14901f073cd308e67641f34c99d6002
Kern <gf4t47@gmail.com>
2018-10-28 23:46:01 -0700

    vga buffer

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,28 @@
 #![no_main]
 
 extern crate bootloader_precompiled;
+extern crate volatile;
+extern crate spin;
+
+#[macro_use]
+extern crate lazy_static;
+
+#[macro_use]
+mod vga_buffer;
 
 use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-//    let vga_buffer = 0xb8000 as *mut u8;
-//
-//    for (i, &byte) in HELLO.iter().enumerate() {
-//        unsafe {
-//            *vga_buffer.offset(i as isize * 2) = byte;
-//            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-//        }
-//    }
+    panic!("Some panic message");
+    println!("Hello World{}", "!");
 
     loop {}
 }
