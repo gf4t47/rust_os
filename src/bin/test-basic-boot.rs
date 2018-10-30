ffa1df63ecef14a405a4639f9f9fd55c793ccb4b
Kern <gf4t47@gmail.com>
2018-10-30 01:48:40 -0700

    integration tests

--- src/bin/test-basic-boot.rs
+++ src/bin/test-basic-boot.rs
@@ -2,26 +2,33 @@
 #![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
 #![cfg_attr(test, allow(dead_code, unused_macros, unused_imports))]
 
-// NEW: Add the library as dependency (same crate name as executable)
+// add the library as dependency (same crate name as executable)
 #[macro_use]
 extern crate rust_os;
 
 use core::panic::PanicInfo;
+use rust_os::exit_qemu;
 
 /// This function is the entry point, since the linker looks for a function
 /// named `_start` by default.
 #[cfg(not(test))]
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+    serial_println!("ok");
 
+    unsafe { exit_qemu(); }
     loop {}
 }
+
 
 /// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
+    serial_println!("failed");
+
+    serial_println!("{}", info);
+
+    unsafe { exit_qemu(); }
     loop {}
 }
